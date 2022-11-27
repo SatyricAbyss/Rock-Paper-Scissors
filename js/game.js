14b1a8a68171042bf081b169830c4c804b707e1a
Jesse Vroegrijk <53167559+SatyricAbyss@users.noreply.github.com>
2022-11-27 17:38:52 +0100

    Update game.js
    Added saving, loading and clearing to the game.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
 let gameTies = 0;
+const userName = localStorage.getItem('userName');
 
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
@@ -29,8 +30,47 @@ const getComputerChoice = () => {
     }
 }
 
+const saveData = () => {
+    localStorage.setItem('playerScore', playerScore);
+    localStorage.setItem('computerScore', computerScore);
+    if(!document.getElementById('userName').value) {
+        localStorage.setItem('userName', 'Unknown');
+    } else {
+        localStorage.setItem('userName', document.getElementById('userName').value);
+    }
+    setUserOnScreen();
+    console.log("Your player data has been saved. "+localStorage.getItem('userName')+": "+playerScore+" and Computer: "+computerScore);
+}
+
+const getData = () => {
+    let user = localStorage.getItem('userName');
+    playerScore = localStorage.getItem('playerScore');
+    computerScore = localStorage.getItem('computerScore');
+    setUserOnScreen();
+    document.getElementById('gameOutcome').innerHTML = `Your data has been loaded.`;
+    document.getElementById('computerChoice').innerHTML = `<strong>The computer</strong> has ${computerScore} victories.`;
+    document.getElementById('playerChoice').innerHTML = `<strong>${user}</strong> has ${playerScore} victories.`;
+    document.getElementById('userName').value = user;
+    console.log("Your game has loaded. "+localStorage.getItem('userName')+" with "+playerScore+" and "+computerScore+" defeats");
+}
+
+const resetData = () => {
+    localStorage.clear();
+    document.getElementById('gameOutcome').innerHTML = `You have reset your data`;
+}
+
+const setUserOnScreen = () => {
+    if(!userName){
+        document.getElementById('welcomeUser').innerHTML = `Welcome new user, please submit a username!`;
+    } else {
+        document.getElementById('welcomeUser').innerHTML = `Welcome ${userName}`;
+    }
+}
+
+
 const determineWinner = (userChoice, computerChoice) => {
     if(userChoice === computerChoice){
+        gameTies++;
         return 'Tie';
     } else if(userChoice === 'rock') {
         if(computerChoice === 'paper'){
